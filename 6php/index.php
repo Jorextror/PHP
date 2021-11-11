74cf3b49d02ab174e02411fc68b8f7d8a770a36f
Jorextror <joliveda@gmail.com>
2021-11-11 17:44:34 +0100

    028

--- 6php/index.php
+++ 6php/index.php
@@ -1,23 +1,32 @@
+<?php
+session_start();
+if (empty($_SESSION["missatge"])){
+    $_SESSION["missatge"]=array("nom"=>"","passw"=>"","formacio"=>"","idiomas"=>"","mail"=>"","web"=>"");
+}
+?>
 <!DOCTYPE html>
 <html lang="ca" >
     <head>
         <meta charset="utf-8" />
         <title>Formulari </title>
+        <style>
+            .error {color: #FF0000;}
+        </style>
     </head>
     <body>
         <h1>Formulari:</h1>
-        
+        <p><span class="error">* required field</span></p>
             <form action="validacio.php" method="POST" enctype="multpart/form-data">
                 <p> 
                 Nom:<label for="nom"></label>
                     <input type="text" name="nom" autofocus 
-                           minlength="2" maxlength="25" 
-                           pattern="[a-zA-Z]+" required="required"
-                           placeholder="Introdueix el teu nom"/><br/>
+                           placeholder="Introdueix el teu nom"/>
+                           <span class="error">* <?php print_r($_SESSION["missatge"]["nom"]);?></span><br/>
                 
                 <p> 
                 Password:<label for="passw"></label>
-                    <input type="password" name="passw" required /><br/>
+                    <input type="password" name="passw" />
+                    <span class="error">* <?php print_r($_SESSION["missatge"]["passw"]);?></span><br/>
                 </p>
                 Formacio:<br/>
                 <Select name="formacio">
@@ -25,6 +34,7 @@
                     <option>FP</option>
                     <option>BAT</option>
                 </Select>
+                <span class="error"><?php print_r($_SESSION["missatge"]["formacio"]);?></span><br/>
                 <br/><br/><br/><br/>
 
                 Idiomas:<input type="checkbox" name="idiomas" value="ca">Català
@@ -32,19 +42,20 @@
                 <input type="checkbox" name="idiomas" value="en">Anglès
                 <input type="checkbox" name="idiomas" value="fr">Francés
                 <input type="checkbox" name="idiomas" value="al">Alemà
+                <span class="error"><?php print_r($_SESSION["missatge"]["idiomas"]);?></span><br/>
 
                 <p>
                     Email:<label for="mail"></label>
-                    <input type="email" name="email" value="Administrador" /><br/>
+                    <input type="email" name="mail" value="Administrador" />
+                    <span class="error">* <?php print_r($_SESSION["missatge"]["mail"]);?></span><br/>
                 </p>
 
                 <p> 
                 Lloc Web:<label for="web"></label>
                     <input type="url" name="web" /><br/>
+                    <span class="error"><?php print_r($_SESSION["missatge"]["web"]);?></span><br/>
                 </p>
                 <input type="submit" value="Enviar"/>
-                <span class="missatge"><?php echo $missatge;?></span>
-
             </form>
         </p>
     </body>
--- 6php/validacio.php
+++ 6php/validacio.php
@@ -1,43 +1,49 @@
 <?php
-$missatge = "ok";
+session_start();
 
-if  (!( empty($_POST['nom']) || empty($_POST['mail']) || empty($_POST['passw']))) {
-        
-        $nom = $_POST['nom'];
-        $passw = $_POST['passw'];
-        $mail = $_POST['mail'];
+if  (isset($_POST['nom']) && isset($_POST['mail']) && isset($_POST['passw'])) {
+    $nom = $_POST['nom'];
+    $passw = $_POST['passw'];
+    $mail = $_POST['mail'];
 
-        if (!(preg_match ("/[A-Za-z ]+/",$nom))) {
-            $missatge =$missatge+ "nom";
-        }
-        if (!preg_match("/^(?=.?[a-zA-Z])(?=.?[0-9])(?=.?[-+]).{8,}$/i",$passw)){
-            $missatge =$missatge+ "pass";
-        }
-
-        if (!(empty($_POST['formacio']))) {
-            $formacio = $_POST['formacio'];
-        }
-
-        if (!(isset($_POST['idiomas']) && $_POST['idiomas'] == '1')){
-            $idiomas = $_POST['idiomas'];
-            $missatge =$missatge+ "idiomas";
-        }
-
-        if (!(preg_match($mail, FILTER_VALIDATE_EMAIL))){
-            $missatge =$missatge+ "mail";
-        }
-        if (!(empty($_POST['web']))){
-            $web = $_POST['web'];
-            if ((count(parse_url($web)))>2){
-                $web = 'http://' + $web;
-            }
-        }
-        
-    } else {
-        $missatge = 'falten_valors';
+    if (!(preg_match ("/[A-Za-z ]+/",$nom))) {
+        $_SESSION["missatge"]["nom"]="Nom nomes letras sisplau";
+    }else{
+        $_SESSION["missatge"]["nom"]="Nom correcte";
+    }
+    if (!preg_match("/^(?=.?[a-zA-Z])(?=.?[0-9])(?=.?[-+]).{8,}$/i",$passw)){
+        $_SESSION["missatge"]["passw"]="Contrasenya no apta(8 caracts minims, una:(mayus,minuscula,enter,carct especial(-/*+)))";
     }
 
-    if ($missatge!= "ok") {
-        header("Location:index.php?missatge=$missatge");
+    if (!(empty($_POST['formacio']))) {
+        $formacio = $_POST['formacio'];
     }
+
+    if (!(isset($_POST['idiomas']) && $_POST['idiomas'] == '1')){
+        $idiomas = $_POST['idiomas'];
+    
+    }
+
+    if (!(preg_match($mail, FILTER_VALIDATE_EMAIL))){
+        $_SESSION["missatge"]["mail"]="Mail no existeix";
+    }
+    if (!(empty($_POST['web']))){
+        $web = $_POST['web'];
+        if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$web)) {
+            $_SESSION["missatge"]["web"]="web URL invalid";
+        }        
+    }
+        
+} else {
+    if  (!isset($_POST['nom'])){
+        $_SESSION["missatge"]["nom"]="Nom obligatori";
+    }
+    if  (!isset($_POST['passw'])){
+        $_SESSION["missatge"]["passw"]="Contrasenya obligatori";
+    }
+    if  (!isset($_POST['mail'])){
+        $_SESSION["missatge"]["mail"]="Mail obligatori";
+    }
+}
+header("Location:index.php");
 ?>
